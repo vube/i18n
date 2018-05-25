d50ad41a11a3dfe2bec4b82e588d5bda7a882b78
Chris Williams <cwilliams@yext.com>
2018-05-25 11:59:55 -0400

    Fix 12-hour formatting for midnight

--- datetime.go
+++ datetime.go
@@ -491,6 +491,9 @@ func (t *Translator) formatDateTimeComponentHour12(datetime time.Time, length in
 	if hour > 12 {
 		hour = hour - 12
 	}
+	if hour == 0 {
+		hour = 12
+	}
 
 	switch length {
 	case datetimeFormatLength1Plus:
